2d82a158313c6c34aefa7b1c90a582a453621363
Ngọc Thắng <ngocthang.devweb@gmail.com>
2023-04-28 16:41:53 +0700

    Push form GUI

--- Nhom9_QuanLiCuaHangXeMay/src/application/GUI.java
+++ Nhom9_QuanLiCuaHangXeMay/src/application/GUI.java
@@ -11,7 +11,7 @@ public class GUI extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
